3e7afe20da0ad036890928203ddec66b8863b0fe
alienworld1 <astronomyguy0030@gmail.com>
2024-02-23 02:28:32 +0530

    Fetch weather data for a location

---
+++ script.js
@@ -0,0 +1,20 @@
+async function fetchWeatherData(location = 'Paris') {
+    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=7af347fc3b4e4c5fbfb203652242202&q=${location}`)
+
+    const data = await response.json();
+    return data.current;
+}
+
+async function filterWeatherData(location) {
+    const weather = await fetchWeatherData(location);
+    const requiredData = {
+        condition: weather.condition,
+        temp: weather.temp_c,
+    };
+    return requiredData;
+}
+
+window.addEventListener('load', async () => {
+    const data = await filterWeatherData('Paris');
+    console.log(data);
+})
